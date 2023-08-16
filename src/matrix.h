b589a2d1c9b2fbf4f571bf36f202f5f1edd76988
Renzo Battisti <renzo.battisti1@gmail.com>
2023-08-16 13:21:09 -0300

    simplifying

--- src/matrix.h
+++ src/matrix.h
@@ -256,57 +256,38 @@ Matrix* matrix_add( Matrix* m, Matrix* n )
 
 			current_m = current_m->right;
 			current_n = current_n->right;
-		} else if(current_m->column == current_n->column && current_m->column == -1)
-			{
-				current_new = current_new_line_head->below;
-				current_new_line_head = current_new_line_head->below;
-				current_m = current_m->below->right;
-				current_n = current_n->below->right;
-			}
+		if(current_m->column == current_n->column && current_m->column == -1)
+		{
+			current_new = current_new_line_head->below;
+			current_new_line_head = current_new_line_head->below;
+			current_m = current_m->below->right;
+			current_n = current_n->below->right;
+		}
 
 		while(current_m->line < current_n->line) //
 		{
-			current_m = current_m->right;
-			if(current_m->line == -1)
-			{
-				while(current_n->line != -1)
-				{
-					current_new->right = (Matrix*)malloc(sizeof(Matrix));
-					current_new = current_new->right;
+			current_new->right = (Matrix*)malloc(sizeof(Matrix));
+			current_new = current_new->right;
 
-					current_new->right = current_new_line_head; 
-					current_new->line = current_n->line; 
-					current_new->column = current_n->column; 
-					current_new->info = current_n->info;
-				}
-				current_new = current_new_line_head->below;
-				current_new_line_head = current_new_line_head->below;
-				current_m = current_m->below->right;
-				current_n = current_n->below->right;
-			}
+			current_new->right = current_new_line_head; 
+			current_new->line = current_m->line; 
+			current_new->column = current_m->column; 
+			current_new->info = current_m->info;
+
+			current_m = current_m->right;
 		}
 
 		while(current_n->line < current_m->line)
 		{
+			current_new->right = (Matrix*)malloc(sizeof(Matrix));
+			current_new = current_new->right;
+
+			current_new->right = current_new_line_head; 
+			current_new->line = current_n->line; 
+			current_new->column = current_n->column; 
+			current_new->info = current_n->info;
+
 			current_n = current_n->right;
-
-			if(current_n->line == -1)
-			{
-				while(current_m->line != -1)
-				{
-					current_new->right = (Matrix*)malloc(sizeof(Matrix));
-					current_new = current_new->right;
-
-					current_new->right = current_new_line_head; 
-					current_new->line = current_m->line; 
-					current_new->column = current_m->column; 
-					current_new->info = current_m->info;
-				}
-				current_new = current_new_line_head->below;
-				current_new_line_head = current_new_line_head->below;
-				current_m = current_m->below;
-				current_n = current_n->below;
-			}
 		}
 	}
 
