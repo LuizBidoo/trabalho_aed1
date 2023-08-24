e1960e4d4d394f75a508b25dbe06cd8c09b1f2c9
Renzo Battisti <renzo.battisti1@gmail.com>
2023-08-24 14:57:52 -0300

    set funcionando

--- src/mainProvisoria.c
+++ src/mainProvisoria.c
@@ -4,7 +4,7 @@ int main( void ) {
     /* Inicializacao da aplicacao ... */
     Matrix *A = matrix_create();
     matrix_print(A);
-    matrix_setelem(A, 2, 1, 0);
+    matrix_setelem(A, 2, 1, 10);
     matrix_print(A);
     return 0;
 }
