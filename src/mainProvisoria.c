cb68f919f740d596e050abcf6f4ba60d76529c48
Renzo Battisti <renzo.battisti1@gmail.com>
2023-08-25 15:28:24 -0300

    matrix_multiply normal nao funcionando

--- src/mainProvisoria.c
+++ src/mainProvisoria.c
@@ -2,8 +2,14 @@
 
 int main( void ) {
     /* Inicializacao da aplicacao ... */
-    Matrix *A = matrix_create(5);
-    matrix_print(A, 5);
+    Matrix *A = matrix_create(2);
+    printf("A: \n");
+    matrix_print(A, 2);
+    printf("B: \n");
+    Matrix *B = matrix_create(2);
+    matrix_print(B, 2);
+    printf("mult: \n");
+    matrix_print(matrix_multiply(A, B, 2), 2);
     return 0;
 }
 
