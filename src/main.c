91e7a9e400c045cd570b780aaa01b16b16773fed
Renzou1 <114614401+Renzou1@users.noreply.github.com>
2023-08-26 13:25:40 -0300

    main com timer

--- src/main.c
+++ src/main.c
@@ -4,18 +4,18 @@
 int main( void ) {
     time_t start = time(NULL);
     /* Inicializacao da aplicacao ... */
-    Matrix *A = matrix_create_random(500);
-    matrix_print( A );
-    Matrix *B = matrix_create_random(500); 
-    matrix_print( B );
+    Matrix *A = matrix_create_random(5000);
+    //matrix_print( A );
+    Matrix *B = matrix_create_random(5000); 
+    //matrix_print( B );
     Matrix *C = matrix_add( A, B ); 
-    matrix_print( C );
+    //matrix_print( C );
     matrix_destroy( C );
     C = matrix_multiply( A, B ); 
-    matrix_print( C );
+    //matrix_print( C );
     matrix_destroy( C );
     C = matrix_transpose( A ); 
-    matrix_print( C );
+    //matrix_print( C );
     matrix_destroy( C );
     matrix_destroy( A );
     matrix_destroy( B );
