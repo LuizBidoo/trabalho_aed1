ee735c9f18a6f3a0c75cae7a8c1fd58cd986013d
Renzou1 <114614401+Renzou1@users.noreply.github.com>
2023-08-26 13:24:42 -0300

    main com timer

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "matrix.h"
+#include <time.h>
 
 int main( void ) {
+    time_t start = time(NULL);
     /* Inicializacao da aplicacao ... */
-    Matrix *A = matrix_create();
+    Matrix *A = matrix_create_random(500);
     matrix_print( A );
-    Matrix *B = matrix_create(); 
+    Matrix *B = matrix_create_random(500); 
     matrix_print( B );
     Matrix *C = matrix_add( A, B ); 
     matrix_print( C );
@@ -17,6 +19,10 @@ int main( void ) {
     matrix_destroy( C );
     matrix_destroy( A );
     matrix_destroy( B );
+    time_t end = time(NULL);
+    printf("Tempo de execucao: %ld", (end-start));
+
     return 0;
+    
 }
 
