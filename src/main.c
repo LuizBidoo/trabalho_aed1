1b1bf1f0521f0d9ddc46b413e531f21c0e88d6f5
Renzou1 <114614401+Renzou1@users.noreply.github.com>
2023-08-26 21:28:52 -0300

    finalized

--- src/main.c
+++ src/main.c
@@ -6,19 +6,19 @@ int main( void ) {
     /* Inicializacao da aplicacao ... */
     Matrix *A = matrix_create_random(5000);
     //matrix_print( A );
-    Matrix *B = matrix_create_random(5000); 
+    Matrix *B = matrix_create(5000); 
     //matrix_print( B );
-    Matrix *C = matrix_add( A, B ); 
+    Matrix *C = matrix_add( A, B, 5000 ); 
     //matrix_print( C );
-    matrix_destroy( C );
-    C = matrix_multiply( A, B ); 
+    matrix_destroy( C, 5000 );
+    C = matrix_multiply( A, B, 5000 ); 
     //matrix_print( C );
-    matrix_destroy( C );
-    C = matrix_transpose( A ); 
+    matrix_destroy( C, 5000 );
+    C = matrix_transpose( A, 5000 ); 
     //matrix_print( C );
-    matrix_destroy( C );
-    matrix_destroy( A );
-    matrix_destroy( B );
+    matrix_destroy( C, 5000 );
+    matrix_destroy( A, 5000 );
+    matrix_destroy( B, 5000 );
 
     time_t end = time(NULL);
     printf("Tempo de execucao: %ld", (end-start));
