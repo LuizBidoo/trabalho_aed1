25bad13c623096343edfdb9eb7f59a8a3e9c5291
Renzou1 <114614401+Renzou1@users.noreply.github.com>
2023-08-26 21:28:50 -0300

    finalized

--- src/mainProvisoria.c
+++ src/mainProvisoria.c
@@ -4,21 +4,14 @@
 int main( void ) {
     time_t start = time(NULL);
     /* Inicializacao da aplicacao ... */
-    Matrix *A = matrix_create_random(10);
-    matrix_print( A ); 
-    Matrix* B = matrix_add( A, A );
-    matrix_destroy( A );   
-    //matrix_print( C );
-    //matrix_destroy( C );
-    //C = matrix_transpose( A ); 
-    //matrix_print( C );
-    //matrix_destroy( C );
-    
-    
+    Matrix *A = matrix_create_random(5000);
+    Matrix *B = matrix_create_random(5000);
+    matrix_destroy(A);
+    matrix_destroy(B);
+
     time_t end = time(NULL);
     printf("Tempo de execucao: %ld", (end-start));
 
     return 0;
     
 }
-
